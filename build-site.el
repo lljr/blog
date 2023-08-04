5231031d9e2f79919230c54bde433bfcc70163a0
lljr <rostranjj@gmail.com>
2023-08-04 14:48:28 -0600

    Revert "simplify vars"
    This reverts commit c6faaa8ed241997e9934147ec96403795276b1d4.

--- build-site.el
+++ build-site.el
@@ -1,8 +1,8 @@
 ;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
 (require 'package)
-(setq package-user-dir (expand-file-name "./.packages")
-      package-archives '(("melpa" . "https://melpa.org/packages/")
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
 ;; Initialize the package system
@@ -18,18 +18,18 @@
 
 ;; Define the publishing project
 (setq org-publish-project-alist
-      '(("blog"
-         :recursive t
-         :base-directory "./content"
-         :publishing-directory "./public"
-         :publishing-function 'org-html-publish-to-html
+      (list
+       (list "blog"
+             :recursive t
+             :base-directory "./content"
+             :publishing-directory "./public"
+             :publishing-function 'org-html-publish-to-html
 
-         :with-author nil           ;; Don't include author name
-         :with-creator nil
-         :with-toc t
-         :section-numbers nil       ;; Don't include section numbers
-         :time-stamp-file nil))
-      org-html-html5-fancy t
+              :with-author nil           ;; Don't include author name
+             :with-creator t            ;; Include Emacs and Org versions in footer
+             :with-toc t                ;; Include a table of contents
+             :section-numbers nil       ;; Don't include section numbers
+             :time-stamp-file nil))
       org-html-validation-link nil
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
@@ -37,4 +37,5 @@
 
 ;; Generate the site output
 (org-publish-all t)
+
 (message "Build complete!")
